43c7a7b4ee047a924ab292073aaf9df674d6972e
Victor Leon <victorlyonfuentes1998@gmail.com>
2024-02-01 17:01:31 -0400

    first commit

--- dataBase/db.js
+++ dataBase/db.js
@@ -6,6 +6,7 @@ const db = new Sequelize(process.env.BASE_URL,{
     host:process.env.BD_HOST,
     port:process.env.BD_PORT,
     dialect : pg,
+    ssl:true,
     pool:{
         max:5,
         min:0,
