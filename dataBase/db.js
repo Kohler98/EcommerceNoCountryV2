75deead6242a439d40ebe6eff00edbc257bbd189
Victor Leon <victorlyonfuentes1998@gmail.com>
2024-02-01 15:55:43 -0400

    first commit

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,11 +1,11 @@
 const {Sequelize} = require('sequelize')
- 
+const pg = require("pg")
 require('dotenv').config()
 
 const db = new Sequelize(process.env.BASE_URL,{
     host:process.env.BD_HOST,
     port:process.env.BD_PORT,
-    dialect : 'postgres',
+    dialect : pg,
     pool:{
         max:5,
         min:0,
